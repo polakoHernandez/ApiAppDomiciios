d339af67cf880eeef7b29d3fbae0cb5cf8184c4e
polakohernandez <luvaskyhramirez@gmail.com>
2024-01-16 21:36:53 -0500

    Actualizando variables de entorno

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "1065828184";
 export const DB_NAME = process.env.DB_NAME || "railway";
-export const DB_PORT = process.env.DB_NAME || 3306;
+export const DB_PORT = process.env.DB_PORT || 3306;
