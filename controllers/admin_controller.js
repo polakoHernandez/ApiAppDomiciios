2dc0beffebae152de1276dc39dd79c209712c614
polakohernandez <luvaskyhramirez@gmail.com>
2024-01-17 23:37:09 -0500

    corrie¿giendo la respuesta 200

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -37,7 +37,7 @@ export const obtenerAdmin = async (req, res) => {
     if (respuesta[0].length === 0) {
       res.status(404).json({ respuesta: "Usuario no encontrado" });
     } else if (respuesta[0].length !== 0) {
-      res.status(200).json({ admin: respuesta[0] });
+      res.status(200).json({ admin: respuesta[0][0] });
     }
 
     await connection.commit();
