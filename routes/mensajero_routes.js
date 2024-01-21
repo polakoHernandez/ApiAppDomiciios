df30bb7312b793d33e86b3318d6850f8f39cf65b
polakohernandez <luvaskyhramirez@gmail.com>
2024-01-21 00:56:21 -0500

    Modificndo la ruta para obtener mensajero por id

--- routes/mensajero_routes.js
+++ routes/mensajero_routes.js
@@ -4,6 +4,6 @@ import { obtenerMensajero } from "../controllers/mensajero_controllers.js";
 const routerMensajero = Router();
 
 routerAdmnin.get("/listarAdmins", listarAdmins);
-routerAdmnin.get("/obtenerAdmin/:documento", obtenerMensajero);
+routerAdmnin.get("/obtenerMensajero/:documento", obtenerMensajero);
 
 export default routerMensajero;
