9499ca431f9872f57259b47fde79094366ba3337
polakohernandez <luvaskyhramirez@gmail.com>
2024-01-21 01:04:04 -0500

    Eliminando metodo de routerMensajero

--- routes/mensajero_routes.js
+++ routes/mensajero_routes.js
@@ -3,7 +3,6 @@ import { obtenerMensajero } from "../controllers/mensajero_controllers.js";
 
 const routerMensajero = Router();
 
-routerMensajero.get("/listarAdmins", listarAdmins);
 routerMensajero.get("/obtenerMensajero/:documento", obtenerMensajero);
 
 export default routerMensajero;
